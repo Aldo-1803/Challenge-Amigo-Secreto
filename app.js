f439d2249fc88dd03130a12d1e417e70b9c6570d
Aldo Amarilla <AldoAmarilla_@hotmail.com>
2025-02-12 22:39:01 -0300

    Creación de la función "Agregar amigos" para agregarlos a la lista creada.

--- app.js
+++ app.js
@@ -2,3 +2,14 @@
 
 let nombreAmigos = [];
 
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;
+    if ( nombre === '') {
+        alert("Por favor ingrese un nombre");
+    } else {
+        nombreAmigos.push(nombre);
+        limpiarCaja();
+    }
+    console.log(nombre);
+    mostrarLista();
+}
